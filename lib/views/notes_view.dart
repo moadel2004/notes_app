e833195aa4794ec60f1270ea566031d5ac8e5bb9
moadel2004 <mostafaadel2988@gmail.com>
2025-10-20 12:31:07 +0300

    create note bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/AddNoteBottomSheet.dart';
 import 'package:note_app/views/widgets/NotesViewBody.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,7 +9,14 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteBottom();
+            },
+          );
+        },
         backgroundColor: Colors.blue,
         child: Icon(Icons.add),
       ),
